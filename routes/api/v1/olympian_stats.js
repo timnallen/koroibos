635ba2fb993d76875ad8bf790e37583651b9a4d7
Tim Allen <tnelsallen3@gmail.com>
2019-06-20 13:09:49 -0600

    Replace multiple database calls with for loop and formatter for one database call in olympian stats router

--- routes/api/v1/olympian_stats.js
+++ routes/api/v1/olympian_stats.js
@@ -10,20 +10,45 @@ var OlympianEvent = require('../../../models').OlympianEvent;
 router.get('/', async function(req, res, next) {
   res.setHeader("Content-Type", "application/json");
   try {
-    var olympian_stats = {};
-    let competing_olympians = await Olympian.findAll();
-    olympian_stats.total_competing_olympians = competing_olympians.length;
-    let average_male = await Olympian.findAll({ where: { sex: 'M' }, attributes: [[sequelize.fn('AVG', sequelize.col('weight')), 'average_male_weight']] });
-    let average_female = await Olympian.findAll({ where: { sex: 'F' }, attributes: [[sequelize.fn('AVG', sequelize.col('weight')), 'average_female_weight']] });
-    let male_olympians = average_male[0].dataValues.average_male_weight
-    let female_olympians = average_female[0].dataValues.average_female_weight
-    olympian_stats.average_weight = {unit: 'kg', male_olympians: male_olympians, female_olympians: female_olympians};
-    let age_stat = await Olympian.findAll({ attributes: [[sequelize.fn('AVG', sequelize.col('age')), 'average_age']] });
-    olympian_stats.average_age = age_stat[0].dataValues.average_age;
+    var stats = {
+      totalAge: 0,
+      males: 0,
+      females: 0,
+      maleWeight: 0,
+      femaleWeight: 0
+    }
+    var olympians = await Olympian.findAll();
+
+    for (var i = 0; i < olympians.length; i++) {
+      stats.totalAge += olympians[i].age;
+      if (olympians[i].sex === 'M') {
+        stats.maleWeight += olympians[i].weight
+        stats.males++
+      } else {
+        stats.femaleWeight += olympians[i].weight
+        stats.females++
+      }
+    }
+
+    var olympian_stats = statFormatter(olympians, stats);
+
     res.status(200).send({olympian_stats: olympian_stats});
   } catch (error) {
+    console.log(error)
     res.status(500).send({ error });
   }
 });
+
+function statFormatter(olympians, stats) {
+  return {
+    total_competing_olympians: olympians.length,
+    average_weight: {
+      unit: "kg",
+      male_olympians: Math.round(stats.maleWeight / stats.males),
+      female_olympians: Math.round(stats.femaleWeight / stats.females)
+    },
+    average_age: Math.round(stats.totalAge / olympians.length)
+  };
+};
 
 module.exports = router;
